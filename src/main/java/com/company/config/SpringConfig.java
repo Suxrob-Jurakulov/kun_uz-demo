400045df62af3b00357b6c67faff452988f28143
Suxrob-Jurakulov <suxrobbek997@gmail.com>
2023-05-23 16:22:09 +0500

    Changed

--- src/main/java/com/company/config/SpringConfig.java
+++ src/main/java/com/company/config/SpringConfig.java
@@ -1,13 +1,10 @@
 package com.company.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
